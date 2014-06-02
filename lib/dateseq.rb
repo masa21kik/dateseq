4d0a1587bc200f8cd40dbf45afeb03af6da96cc1
masa21kik <masa21kik@gmail.com>
2014-06-03 00:27:19 +0900

    implement dateseq

--- lib/dateseq.rb
+++ lib/dateseq.rb
@@ -1,6 +1,22 @@
 require 'dateseq/version'
+require 'date'
 
 # Date Sequence
 module Dateseq
-  # Your code goes here...
+  # Date Sequence generator
+  class Generator
+    def initialize(options)
+      @format = options[:format] || '%Y%m%d'
+    end
+
+    def sequence(from_date, to_date)
+      from = Date.strptime(from_date, @format)
+      to = Date.strptime(to_date, @format)
+      seq = []
+      from.upto(to).each do |date|
+        seq << date.strftime(@format)
+      end
+      seq
+    end
+  end
 end
--- spec/dateseq_spec.rb
+++ spec/dateseq_spec.rb
@@ -4,4 +4,29 @@ describe Dateseq do
   it 'should have a version number' do
     expect(Dateseq::VERSION).not_to be_nil
   end
+
+  shared_examples_for 'generate date sequence' do
+    it 'generate date sequence' do
+      seq = generator.sequence(from, to)
+      expect(seq).to eq expected
+    end
+  end
+
+  let(:generator) { Dateseq::Generator.new(options) }
+
+  context '20131230 to 20140101' do
+    let(:from) { '20131230' }
+    let(:to) { '20140101' }
+    let(:options) { { format: '%Y%m%d' } }
+    let(:expected) { %w(20131230 20131231 20140101) }
+    it_behaves_like 'generate date sequence'
+  end
+
+  context '2013-12-30 to 2014-01-01' do
+    let(:from) { '2013-12-30' }
+    let(:to) { '2014-01-01' }
+    let(:options) { { format: '%Y-%m-%d' } }
+    let(:expected) { %w(2013-12-30 2013-12-31 2014-01-01) }
+    it_behaves_like 'generate date sequence'
+  end
 end
