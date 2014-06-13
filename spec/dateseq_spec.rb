9a37e9ee5b9493296837caee9f32d3ac70a4523a
masa21kik <masa21kik@gmail.com>
2014-06-13 23:41:46 +0900

    add test cases

--- spec/dateseq_spec.rb
+++ spec/dateseq_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Dateseq do
+describe Dateseq::Generator do
   it 'should have a version number' do
     expect(Dateseq::VERSION).not_to be_nil
   end
@@ -12,21 +12,48 @@ describe Dateseq do
     end
   end
 
-  let(:generator) { Dateseq::Generator.new(options) }
-
-  context '20131230 to 20140101' do
-    let(:from) { '20131230' }
-    let(:to) { '20140101' }
-    let(:options) { { format: '%Y%m%d' } }
-    let(:expected) { %w(20131230 20131231 20140101) }
-    it_behaves_like 'generate date sequence'
+  shared_examples_for 'generate date sequence_str' do
+    it 'generate date sequence_str' do
+      seq = generator.sequence_str(from, to)
+      expect(seq).to eq expected
+    end
   end
 
-  context '2013-12-30 to 2014-01-01' do
-    let(:from) { '2013-12-30' }
-    let(:to) { '2014-01-01' }
-    let(:options) { { format: '%Y-%m-%d' } }
-    let(:expected) { %w(2013-12-30 2013-12-31 2014-01-01) }
-    it_behaves_like 'generate date sequence'
+  let(:generator) { described_class.new(options) }
+
+  describe '#sequence' do
+    context '20131230 to 20140101' do
+      let(:from) { '20131230' }
+      let(:to) { '20140101' }
+      let(:options) { { format: '%Y%m%d' } }
+      let(:expected) { %w(20131230 20131231 20140101) }
+      it_behaves_like 'generate date sequence'
+    end
+
+    context '2013-12-30 to 2014-01-01' do
+      let(:from) { '2013-12-30' }
+      let(:to) { '2014-01-01' }
+      let(:options) { { format: '%Y-%m-%d' } }
+      let(:expected) { %w(2013-12-30 2013-12-31 2014-01-01) }
+      it_behaves_like 'generate date sequence'
+    end
+  end
+
+  describe '#sequence_str' do
+    context '20131230 to 20140101' do
+      let(:from) { '20131230' }
+      let(:to) { '20140101' }
+      let(:options) { { format: '%Y%m%d' } }
+      let(:expected) { "20131230\n20131231\n20140101" }
+      it_behaves_like 'generate date sequence_str'
+    end
+
+    context '2013/12/30 to 2014/01/01' do
+      let(:from) { '2013/12/30' }
+      let(:to) { '2014/01/01' }
+      let(:options) { { format: '%Y/%m/%d', sep: "\t" } }
+      let(:expected) { "2013/12/30\t2013/12/31\t2014/01/01" }
+      it_behaves_like 'generate date sequence_str'
+    end
   end
 end
